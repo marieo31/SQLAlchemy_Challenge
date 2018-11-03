5a03941c20007546d6d44dc7268276dc9609b67e
MariO <40508635+marieo31@users.noreply.github.com>
2018-11-03 00:24:28 -0700

    all done : I hope

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
+import datetime as dt
 
 #################################################
 # Database Setup
@@ -39,15 +40,16 @@ def home():
     """List all available api routes."""
     return (
         f"Available Routes:<br/>"
-        f"/api/v1.0/precipitation/<date><br/>"
-        f"/api/v1.0/passengers"
+        f"/api/v1.0/precipitation/date<br/>"
+        f"/api/v1.0/stations"
     )
 
 @app.route("/api/v1.0/precipitation/<date>")
 def precipitation(date):
-    """
+    """    
     Convert the query results to a Dictionary using date as the key and prcp as the value.    
     Return the JSON representation of your dictionary.
+    exemple: http://127.0.0.1:5000/api/v1.0/precipitation/2017-07-15
     """
     
     dico = {date: session.query(Measurement.prcp).filter( Measurement.date == date).all()}       
@@ -66,40 +68,96 @@ def stations():
 #     print(q )
     return jsonify(q)
 
+@app.route("/api/v1.0/tobs")
+def tobs():
+    """
+    # query for the dates and temperature observations from a year from the last data point.
+    # Return a JSON list of Temperature Observations (tobs) for the previous year.
+    """
+    # Calculate the date 1 year ago from the last data point in the database
+    latest_date = session.query(Measurement.date).\
+                order_by(Measurement.date.desc()).\
+                first()
+    # converting the string into datetime
+    dt_latest_date = dt.datetime.strptime(latest_date.date, '%Y-%m-%d')
+    # Calculating the date from 12 months before
+    year_before = dt_latest_date - dt.timedelta(days=365) #20) #365)
+
+    # Perform a query to retrieve the data and precipitation scores
+    prcp_last12m = session.query(Measurement.date, Measurement.prcp).\
+                    filter( Measurement.date >= year_before).all()
+    return jsonify(prcp_last12m)
+
+
+@app.route("/api/v1.0/<start_date>")
+def start_only(start_date):
+    """
+    # Return a JSON list of the minimum temperature, the average temperature, 
+    # and the max temperature for a given start or start-end range.
+    # When given the start only, calculate TMIN, TAVG, and TMAX for all dates greater than and equal to the start date.
+    
+    http://127.0.0.1:5000/api/v1.0/2017-07-15
+    """
+    end_date = session.query(Measurement.date).\
+            order_by(Measurement.date.desc()).first()[0]
+
+    return jsonify(range_normals(start_date, end_date))
+
+@app.route("/api/v1.0/<start_date>/<end_date>")
+def start_end(start_date, end_date): #=None):
+    """
+    # Return a JSON list of the minimum temperature, the average temperature, 
+    # and the max temperature for a given start or start-end range.
+    # When given the start and the end date, calculate the TMIN, TAVG, and TMAX for dates between the start and end date inclusive.
+     example: http://127.0.0.1:5000/api/v1.0/2017-07-15/2017-07-30
+
+    """
+     
+     
+    return jsonify(range_normals(start_date, end_date))
+
+
+def range_normals(trip_start, trip_end):
+    """Range_normals
+    
+    Args:
+        trip_start (str): A date string in the format '%yyyy-%m-%d'
+        trip_end (str): A date string in the format '%yyyy-%m-%d'        
+         
+    Returns:
+        A list of tuples containing the daily normals, tmin, tavg, and tmax
+    
+    """
+    
+    # create the list of dates between trip_start and trip_end
+    d1 = dt.date(int(trip_start[0:4]), int(trip_start[5:7]), int(trip_start[8:]))  # start date
+    d2 = dt.date(int(trip_end[0:4]), int(trip_end[5:7]), int(trip_end[8:]))   # end date
+    delta = d2 - d1         # timedelta
+    trip_dates = [d1 + dt.timedelta(ii) for ii in range(delta.days + 1)]
+    
+    # calculate the normals for theses dates
+    normals = [daily_normals(str(dd)[5:]) for dd in trip_dates ]
+    
+    
+    return normals
+# 
+# 
+def daily_normals(date):
+    """Daily Normals.
+     
+    Args:
+        date (str): A date string in the format '%m-%d'
+         
+    Returns:
+        A list of tuples containing the daily normals, tmin, tavg, and tmax
+     
+    """
+     
+    sel = [func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)]
+    return session.query(*sel).filter(func.strftime("%m-%d", Measurement.date) == date).all()
+
 
 
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-
-# 
-
-# 
-
-# 
-
-# 
-# /api/v1.0/tobs
-# 
-# query for the dates and temperature observations from a year from the last data point.
-# Return a JSON list of Temperature Observations (tobs) for the previous year.
-# 
-# 
-# 
-# /api/v1.0/<start> and /api/v1.0/<start>/<end>
-# 
-# 
-# Return a JSON list of the minimum temperature, the average temperature, and the max temperature for a given start or start-end range.
-# When given the start only, calculate TMIN, TAVG, and TMAX for all dates greater than and equal to the start date.
-# When given the start and the end date, calculate the TMIN, TAVG, and TMAX for dates between the start and end date inclusive.
-# 
-# 
-# 
-# 
-# 
-# Hints
-# 
-# 
-# You will need to join the station and measurement tables for some of the analysis queries.
-# Use Flask jsonify to convert your API data into a valid JSON response object.
